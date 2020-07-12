e2d0035a6fbeda236f2fe8f801cc525423163a7f
vincent_chu <vincent_chu@asus.com>
2020-07-12 15:24:49 +0800

    20200712 Fix kernel panic due to read function of UDP port

--- my_module/packet_related_using_netfilter/drop_UDP_packet/drop_UDP.c
+++ my_module/packet_related_using_netfilter/drop_UDP_packet/drop_UDP.c
@@ -16,20 +16,24 @@
 
 static struct proc_dir_entry *drop_udp_dir;
 static struct proc_dir_entry *drop_count_file;
+static struct proc_dir_entry *port_file;
 static uint32_t count = 0;
+static uint32_t port = 0;
 
-static int drop_file_show(struct seq_file *sf, void *data) {
+//For /proc/drop_udp/drop_count +++
+static int drop_count_show(struct seq_file *sf, void *data) {
      seq_printf(sf, "%d\n", count);
      return 0;
 }
 
-static int drop_file_open(struct inode *inode, struct file *file) {
-    return single_open(file, drop_file_show, NULL);
+static int drop_count_open(struct inode *inode, struct file *file) {
+    return single_open(file, drop_count_show, NULL);
 }
 
+#if 0
 //user會用ubuf告知想讀多少data
 //kernel要負責把user要讀的data大小塞回給user，並回傳user說我塞了多少給你
-static ssize_t drop_file_read(struct file *file, char __user *ubuf, size_t ubuf_size, loff_t *pos) {
+static ssize_t drop_count_read(struct file *file, char __user *ubuf, size_t ubuf_size, loff_t *pos) {
     char buf[256]; //kernel的buffer
     int len = 0;
     
@@ -46,16 +50,63 @@ static ssize_t drop_file_read(struct file *file, char __user *ubuf, size_t ubuf_
     *pos = len;
     return len;
 }
+#endif
 
-#if 1
-static const struct file_operations drop_file_ops =
+static const struct file_operations drop_count_ops =
 {
     .owner = THIS_MODULE,
-    .open = drop_file_open,
+    .open = drop_count_open,
     .read = seq_read,
-    //.write = drop_file_write, //不提供write
 };
-#endif
+//For /proc/drop_udp/drop_count ---
+
+
+
+
+//For /proc/drop_udp/port +++
+
+static int port_show(struct seq_file *sf, void *data) {
+     seq_printf(sf, "%u\n", port);
+     return 0;
+}
+
+static int port_open(struct inode *inode, struct file *file) {
+    return single_open(file, port_show, NULL);
+}
+
+static ssize_t port_write(struct file *file, const char __user *ubuf, size_t ubuf_size, loff_t *pos) {
+    char buf[128];
+    int num, len;
+
+    printk("Enter port_write\n");
+    //確認是否是第一次call write(pos=0), 並且確認是否user提供的buffer size是否小於我們要回傳的buffer size
+    if (*pos > 0 || ubuf_size > 1024)
+        return -EFAULT; //end of file
+
+    if (copy_from_user(buf, ubuf, ubuf_size)) {
+        return -EFAULT;
+    }
+
+    num = sscanf(buf, "%u", &port);
+    //printk("num=%d\n", num);
+
+    if (num != 1) //只允許一個值
+        return -EFAULT;
+
+    len = strlen(buf);
+    *pos = len; //紀錄已經write過
+
+    return len;
+}
+
+static const struct file_operations port_ops =
+{
+    .owner = THIS_MODULE,
+    .open  = port_open,
+    .read = seq_read,
+    .write = port_write, //自己定義write
+};
+//For /proc/drop_udp/port ---
 
 static unsigned int vincent_drop_udp_output(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
     struct iphdr *iph = ip_hdr(skb);
@@ -91,7 +142,7 @@ static int __init vincent_drop_udp_init(void)
     int err = 0;
     printk("Enter vincent_drop_UDP_init");
 
-    //註冊netfilter hook function
+    //Step1: 註冊netfilter hook function
     err = nf_register_net_hook(&init_net, &vincent_drop_udp_ops);
 
     if (err < 0) {
@@ -99,21 +150,31 @@ static int __init vincent_drop_udp_init(void)
         return err;
     }
 
-    //建立目錄
+    //Step2: 建立 "/proc/drop_udp/" 目錄
     drop_udp_dir = proc_mkdir("drop_udp", NULL);
+
     if (!drop_udp_dir) {
         printk(KERN_ERR "%s Fail to create drop udp dir\n", __func__);
         return 1;
     }
-#if 1
-    //建立檔案
-    drop_count_file = proc_create("drop_count", 0644, drop_udp_dir, &drop_file_ops); //也可以用proc_create_data，多一個參數
+
+    //Step3: 建立 "/proc/drop_udp/drop_count" 檔，用於讀取現在drop多少data (也可以用proc_create_data，多一個參數)
+    //第三個參數代表parent dir，也就是要在哪個資料夾之下建立新資料夾，如果是在/proc目錄下建立資料夾，parent為NULL(直接用proc_mkdir)
+    drop_count_file = proc_create("drop_count", 0644, drop_udp_dir, &drop_count_ops);
 
     if (!drop_count_file) {
         printk(KERN_ERR "%s Fail to create file: drop_file\n", __func__);
         return 1;
     }
-#endif
+
+    //Step4: 建立 "/proc/drop_udp/port" 檔，用於讓user指定想要drop的port
+    port_file = proc_create("drop_port", 0666, drop_udp_dir, &port_ops);
+
+    if (!port_file) {
+        printk(KERN_ERR "%s Fail to create file: port_file\n", __func__);
+        return 1;
+    }
+
     return 0;
 }
 
--- my_module/packet_related_using_netfilter/drop_UDP_packet/drop_UDP.mod
+++
@@ -1,2 +0,0 @@
-/home/vincent/side_work/my_module/packet_related_using_netfilter/drop_UDP_packet/drop_UDP.o
-
