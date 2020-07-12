849d63a0c7d64a8210c097be1b9d2320b7c8a6cc
vincent_chu <vincent_chu@asus.com>
2020-07-12 23:13:58 +0800

    Add "DEFINE_SHOW_ATTRIBUTE" support

--- my_module/packet_related_using_netfilter/drop_UDP_packet/drop_UDP.c
+++ my_module/packet_related_using_netfilter/drop_UDP_packet/drop_UDP.c
@@ -18,16 +18,12 @@ static struct proc_dir_entry *drop_udp_dir;
 static struct proc_dir_entry *drop_count_file;
 static struct proc_dir_entry *port_file;
 static uint32_t count = 0;
-static uint32_t port = 0;
+static uint32_t *port;
 
 //For /proc/drop_udp/drop_count +++
 static int drop_count_show(struct seq_file *sf, void *data) {
      seq_printf(sf, "%d\n", count);
      return 0;
-}
-
-static int drop_count_open(struct inode *inode, struct file *file) {
-    return single_open(file, drop_count_show, NULL);
 }
 
 #if 0
@@ -50,23 +46,35 @@ static ssize_t drop_count_read(struct file *file, char __user *ubuf, size_t ubuf
     *pos = len;
     return len;
 }
-#endif
 
-static const struct file_operations drop_count_ops =
+static int drop_count_show(struct seq_file *sf, void *data) {
+     seq_printf(sf, "%d\n", count);
+     return 0;
+}
+
+static int drop_count_open(struct inode *inode, struct file *file) {
+    return single_open(file, drop_count_show, NULL);
+}
+
+static const struct file_operations drop_count_fops =
 {
     .owner = THIS_MODULE,
     .open = drop_count_open,
     .read = seq_read,
 };
+#endif
+
+//如果對於drop_count每次都只想讀，除了drop_count_show仍然要自己實現之外，其餘全部操作可用DEFINE_SHOW_ATTRIBUTE(drop_count)取代
+DEFINE_SHOW_ATTRIBUTE(drop_count);
+
 //For /proc/drop_udp/drop_count ---
 
 
 
 
 //For /proc/drop_udp/port +++
-
 static int port_show(struct seq_file *sf, void *data) {
-     seq_printf(sf, "%u\n", port);
+     seq_printf(sf, "%u\n", *port);
      return 0;
 }
 
@@ -77,7 +85,7 @@ static int port_open(struct inode *inode, struct file *file) {
 static ssize_t port_write(struct file *file, const char __user *ubuf, size_t ubuf_size, loff_t *pos) {
     char buf[128];
     int num, len;
-
+    port = kmalloc(sizeof(uint32_t), GFP_KERNEL); //dynamic allocate
     printk("Enter port_write\n");
     //確認是否是第一次call write(pos=0), 並且確認是否user提供的buffer size是否小於我們要回傳的buffer size
     if (*pos > 0 || ubuf_size > 1024)
@@ -87,7 +95,7 @@ static ssize_t port_write(struct file *file, const char __user *ubuf, size_t ubu
         return -EFAULT;
     }
 
-    num = sscanf(buf, "%u", &port);
+    num = sscanf(buf, "%u", port);
     //printk("num=%d\n", num);
 
     if (num != 1) //只允許一個值
@@ -99,7 +107,7 @@ static ssize_t port_write(struct file *file, const char __user *ubuf, size_t ubu
     return len;
 }
 
-static const struct file_operations port_ops =
+static const struct file_operations port_fops =
 {
     .owner = THIS_MODULE,
     .open  = port_open,
@@ -160,7 +168,7 @@ static int __init vincent_drop_udp_init(void)
 
     //Step3: 建立 "/proc/drop_udp/drop_count" 檔，用於讀取現在drop多少data (也可以用proc_create_data，多一個參數)
     //第三個參數代表parent dir，也就是要在哪個資料夾之下建立新資料夾，如果是在/proc目錄下建立資料夾，parent為NULL(直接用proc_mkdir)
-    drop_count_file = proc_create("drop_count", 0644, drop_udp_dir, &drop_count_ops);
+    drop_count_file = proc_create("drop_count", 0644, drop_udp_dir, &drop_count_fops);
 
     if (!drop_count_file) {
         printk(KERN_ERR "%s Fail to create file: drop_file\n", __func__);
@@ -168,7 +176,7 @@ static int __init vincent_drop_udp_init(void)
     }
 
     //Step4: 建立 "/proc/drop_udp/port" 檔，用於讓user指定想要drop的port
-    port_file = proc_create("drop_port", 0666, drop_udp_dir, &port_ops);
+    port_file = proc_create("drop_port", 0666, drop_udp_dir, &port_fops);
 
     if (!port_file) {
         printk(KERN_ERR "%s Fail to create file: port_file\n", __func__);
